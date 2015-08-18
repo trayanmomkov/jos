b7cd96872b06f411b4912b848683d06bc514fe78
Munzeo.com <info@munzeo.com>
2015-08-18 11:27:23 +0300

    Init

---
+++ jos/src/main/java/info/trekto/jos/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package info.trekto.jos;
+
+/**
+ * @author Trayan Momkov
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
