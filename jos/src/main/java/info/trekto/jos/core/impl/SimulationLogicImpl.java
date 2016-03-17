b955e79a421e9521dcc36d9bbe8b916c2a69d006
Trayan Momkov <info@munzeo.com>
2016-03-18 01:34:41 +0200

    Write object in format version 1 [NOT COMPLETE]

--- jos/src/main/java/info/trekto/jos/core/impl/SimulationLogicImpl.java
+++ jos/src/main/java/info/trekto/jos/core/impl/SimulationLogicImpl.java
@@ -1,14 +1,14 @@
 package info.trekto.jos.core.impl;
 
+import info.trekto.jos.core.Simulation;
+import info.trekto.jos.core.SimulationLogic;
+import info.trekto.jos.model.SimulationObject;
+
 import java.util.Iterator;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import info.trekto.jos.core.Simulation;
-import info.trekto.jos.core.SimulationLogic;
-import info.trekto.jos.model.SimulationObject;
 
 /**
  * @author Trayan Momkov
@@ -17,7 +17,6 @@ import info.trekto.jos.model.SimulationObject;
 public class SimulationLogicImpl implements SimulationLogic {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Override
     public void calculateNewValues(Simulation simulation, List<SimulationObject> targetObjects) {
         logger.warn("calculateNewValues() not implemented");
         for (Iterator iterator = targetObjects.iterator(); iterator.hasNext();) {
