1417ebc75857530fdbd6ee3445ea2fc495b91302
Trayan Momkov <trayan.momkov@gmail.com>
2022-01-18 18:08:22 +0200

    Initialize constants.

--- src/main/java/info/trekto/jos/formulas/ScientificConstants.java
+++ src/main/java/info/trekto/jos/formulas/ScientificConstants.java
@@ -7,17 +7,12 @@ import info.trekto.jos.numbers.New;
  * 3 Mar 2016
  */
 public class ScientificConstants {
-    public static double GRAVITY;
-    public static double PI;
+    public static double GRAVITY = 0.00000000006674; // 6.674×10^−11 N⋅m2/kg2
+    public static double PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
     public static final long NANOSECONDS_IN_ONE_SECOND = 1000 * 1000 * 1000;
     public static final long MILLISECONDS_IN_ONE_SECOND = 1000;
     public static final long NANOSECONDS_IN_ONE_MILLISECOND = 1000 * 1000;
     public static final long MILLI_IN_DAY = 24 * 60 * 60 * MILLISECONDS_IN_ONE_SECOND;
     public static final long MILLI_IN_HOUR = 60 * 60 * MILLISECONDS_IN_ONE_SECOND;
     public static final long MILLI_IN_MINUTE = 60 * MILLISECONDS_IN_ONE_SECOND;
-
-    public static void setConstants() {
-        GRAVITY = 0.00000000006674; // 6.674×10^−11 N⋅m2/kg2
-        PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
-    }
 }
